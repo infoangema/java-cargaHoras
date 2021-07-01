4d8ea9bb27aeb06f3ee730bd85da9b5e3b9d3824
nimarchi@alumno.unlam.edu.ar <nimarchi@alumno.unlam.edu.ar>
2021-07-01 16:36:02 -0300

    Modificaciones

--- src/main/java/com/angema/hours/app/feature/user/models/User.java
+++ src/main/java/com/angema/hours/app/feature/user/models/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
