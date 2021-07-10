49c3b5bc6eda860c643b49cfd7d5d75cd405ed6d
paletGerardo <paletgerardo@gmail.com>
2021-07-10 19:01:02 -0300

    - Mejoras en handlerError.

--- src/main/java/com/angema/hours/app/core/Messages.java
+++ src/main/java/com/angema/hours/app/core/Messages.java
@@ -4,4 +4,5 @@ public class Messages {
 
     public static final String ERROR_ID_CHARACTER = "El parametro ID no puede contener un caracter.";
     public static final String ERROR_USER_NOT_FOUND = "Usuario no encontrado.";
+    public static final String ERROR_MAIL = "El mail debe contener un minimo de 8 y un maximo de 30 caracteres.";
 }
--- src/main/java/com/angema/hours/app/core/errors/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/angema/hours/app/core/errors/RestResponseEntityExceptionHandler.java
@@ -1,28 +1,39 @@
-package com.angema.hours.app.core;
+package com.angema.hours.app.core.errors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value= {Exception.class, IllegalStateException.class})
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
 
-        int status = ((ResponseStatusException) ex).getRawStatusCode();
-        String bodyOfResponse = ((ResponseStatusException) ex).getReason();
-
+        Integer status = ((ResponseStatusException) ex).getRawStatusCode();
+        Error err = new Error();
+        err.message = ((ResponseStatusException) ex).getReason();
+        err.status = String.valueOf(status);
+        err.path = ((ServletWebRequest)request).getRequest().getRequestURI().toString();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        err.timestamp = LocalDateTime.now().format(formatter);
+        log.error(">>> " + err.toString());
         return handleExceptionInternal(
                 ex,
-                bodyOfResponse,
+                err,
                 new HttpHeaders(),
-                HttpStatus.valueOf(status),
+                HttpStatus.valueOf(400),
                 request
         );
     }
