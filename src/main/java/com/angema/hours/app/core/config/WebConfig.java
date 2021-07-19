8eb99766eb208d7ed48e335d4150b33cf9315b83
nimarchi@alumno.unlam.edu.ar <nimarchi@alumno.unlam.edu.ar>
2021-07-19 09:37:01 -0300

    version 0.0.2

--- src/main/java/com/angema/hours/app/core/config/WebConfig.java
+++ src/main/java/com/angema/hours/app/core/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.angema.hours.app.core;
+package com.angema.hours.app.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
