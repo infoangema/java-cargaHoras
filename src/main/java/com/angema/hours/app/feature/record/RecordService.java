88e6a74a0d366b4a35e7ea7e1b41c6b0a8dc2db8
paletgerardo <paletgerardo@hotmail.com>
2021-10-07 11:08:35 -0300

    version 0.0.2

--- src/main/java/com/angema/hours/app/feature/record/RecordService.java
+++ src/main/java/com/angema/hours/app/feature/record/RecordService.java
@@ -122,7 +122,7 @@ public class RecordService {
         List<Record> listFilter = recordRepository.findByListUser(idUser, idProject);
         if (datefrom != null && listFilter.size() != 0) {
             if (dateto != null) {
-                if (datefrom.compareTo(dateto) < 0) {
+                if (datefrom.compareTo(dateto) > 0) {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Messages.ERROR_VALIDATION_DATE);
                 }
                 return listFilter.stream().filter(x -> (x.getDate().compareTo(datefrom) >= 0 && (x.getDate().compareTo(dateto) <= 0)) ).collect(Collectors.toList());
