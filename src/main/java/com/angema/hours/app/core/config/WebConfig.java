af814afffd2ab4ddd804241eb8d543b81558de3e
paletgerardo <paletgerardo@hotmail.com>
2021-10-19 10:54:30 -0300

    mensaje cardif

--- src/main/java/com/angema/hours/app/core/config/WebConfig.java
+++ src/main/java/com/angema/hours/app/core/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.angema.hours.app.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
@@ -15,6 +17,19 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
 
